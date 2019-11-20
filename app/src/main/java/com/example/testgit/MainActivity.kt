2001f072410abb9c9ddd46a0d4a3a1e1546e4c54
Laijiaxing3219 <1536319153@qq.com>
2019-11-20 18:55:36 +0800

    123

---
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun a() {
+        System.out.println("123");
+    }
+}
